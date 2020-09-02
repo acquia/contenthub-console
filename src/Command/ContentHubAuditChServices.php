dfbaae32ad72e17151a1b8da272e7a2aaddfcd52
Ateee89 <attila.tilman@cheppers.com>
2020-09-02 18:00:21 +0200

    LCH-4281: Command for checking deprecated services. (#59)
    * LCH-4281: Add audit command for checking 1.x services.
    
    * LCH-4281: Extend command.
    
    * LCH-4281: Refactor command and add extra trait.

---
+++ src/Command/ContentHubAuditChServices.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Acquia\Console\ContentHub\Command;
+
+use Acquia\Console\ContentHub\Client\ContentHubCommandBase;
+use Acquia\Console\ContentHub\Command\Helpers\DirectoryIteratorTrait;
+use Acquia\Console\ContentHub\Exception\ContentHubVersionException;
+use EclipseGc\CommonConsole\Command\PlatformBootStrapCommandInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ContentHubAuditChServices.
+ *
+ * @package Acquia\Console\ContentHub\Command
+ */
+class ContentHubAuditChServices extends ContentHubCommandBase implements PlatformBootStrapCommandInterface {
+
+  use DirectoryIteratorTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $defaultName = 'ach:audit:ch-services';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configure() {
+    $this->setDescription('Audit deprecated 1.x service usage');
+    $this->setAliases(['ach-serv']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initialize(InputInterface $input, OutputInterface $output) {
+    parent::initialize($input, $output);
+    if ($this->achClientService->getVersion() !== 1) {
+      throw new ContentHubVersionException(1);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $output->writeln('Checking deprecated service usage in modules...');
+    $files = [];
+
+    try {
+      $regex = $this->getFilesInfo('/^((?!acquia_contenthub).)*$/', 'modules');
+    } catch (\Exception $exception) {
+      $output->writeln($exception->getMessage());
+      return 0;
+    }
+
+    $regex_pattern = $this->getRegexPattern();
+    foreach ($regex as $file) {
+      $content = file_get_contents($file[0]);
+      if (preg_match_all("/$regex_pattern/", $content, $matches)) {
+        $files[] = [$file[0], implode(', ', $matches[0])];
+      }
+    }
+
+    if (!$files) {
+      $output->writeln('No deprecated service usage found!');
+      return 0;
+    }
+
+    $output->writeln('Following deprecated ACH services found.');
+    $table = new Table($output);
+    $table->setHeaders(['File', 'Matches found']);
+    $table->addRows($files);
+    $table->render();
+
+    return 1;
+  }
+
+  /**
+   * @return string
+   *   Returns a regex pattern which search for ACH services.
+   */
+  protected function getRegexPattern(): string {
+    return implode('|', $this->getDeprecatedServices());
+  }
+
+  /**
+   * Returns all ACH services from container.
+   *
+   * @return array
+   *   String[] which contains deprecated ach services.
+   */
+  protected function getDeprecatedServices(): array {
+    $deprecated_services = [];
+
+    $container = \Drupal::getContainer();
+    $kernel = $container->get('kernel');
+    $services = $kernel->getCachedContainerDefinition()['services'];
+    foreach ($services as $service_id => $value) {
+      $service_definition = unserialize($value);
+      if (strpos($service_definition['class'], 'acquia_contenthub')) {
+        $deprecated_services[] = $service_definition['properties']['_serviceId'];
+      }
+    }
+
+    return $deprecated_services;
+  }
+
+}
---
+++ src/Command/Helpers/DirectoryIteratorTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Acquia\Console\ContentHub\Command\Helpers;
+
+/**
+ * Trait DirectoryIteratorTrait.
+ *
+ * @package Acquia\Console\ContentHub\Command\Helpers
+ */
+trait DirectoryIteratorTrait {
+
+  /**
+   * Returns RegexIterator filtered by given pattern.
+   *
+   * @param string $pattern
+   *   Regex pattern for filtering files.
+   * @param string $docroot_directory
+   *   Directory name as a string within site path.
+   *   (modules, modules/contrib, etc...)
+   *
+   * @return \RegexIterator
+   *   Returns iterable with file info.
+   * @throws \Exception
+   */
+  public function getFilesInfo(string $pattern, string $docroot_directory): \RegexIterator {
+    $kernel = \Drupal::service('kernel');
+    $directories = [
+      $kernel->getAppRoot(),
+      "{$kernel->getAppRoot()}/{$kernel->getSitePath()}",
+    ];
+
+    foreach ($directories as $directory) {
+      if (!file_exists("$directory/$docroot_directory")) {
+        continue;
+      }
+
+      $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator("$directory/$docroot_directory"));
+    }
+
+    if (!isset($iterator)) {
+      throw new \Exception("Cannot find directory: $directory/$docroot_directory");
+    }
+
+    return new \RegexIterator($iterator, $pattern, \RecursiveRegexIterator::GET_MATCH);
+  }
+
+}
