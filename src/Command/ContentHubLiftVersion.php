cdb259e1f985365082f8237ca413d7e0e9a51c98
Amandeep Singh <arshgrover32@gmail.com>
2020-11-20 20:11:44 +0530

    LCH-4612: As a CH engineer, I need to write a test for ContentHubLiftVersion (#89)

--- src/Command/ContentHubLiftVersion.php
+++ src/Command/ContentHubLiftVersion.php
@@ -2,10 +2,10 @@
 
 namespace Acquia\Console\ContentHub\Command;
 
+use Acquia\Console\ContentHub\Client\ContentHubCommandBase;
 use Acquia\Console\ContentHub\Command\Helpers\PlatformCmdOutputFormatterTrait;
 use Acquia\Console\ContentHub\Command\Helpers\PlatformCommandExecutionTrait;
 use EclipseGc\CommonConsole\Command\PlatformBootStrapCommandInterface;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @package Acquia\Console\ContentHub\Command
  */
-class ContentHubLiftVersion extends Command  implements PlatformBootStrapCommandInterface {
+class ContentHubLiftVersion extends ContentHubCommandBase  implements PlatformBootStrapCommandInterface {
 
   use PlatformCmdOutputFormatterTrait;
   use PlatformCommandExecutionTrait;
@@ -27,13 +27,6 @@ class ContentHubLiftVersion extends Command  implements PlatformBootStrapCommand
    */
   protected static $defaultName = 'ach:lift-version';
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getPlatformBootstrapType(): string {
-    return 'drupal8';
-  }
-
   protected function configure() {
     $this->setDescription('Checks for the Acquia Lift module 4.x version.');
     $this->addOption('clear-cache','cr',InputOption::VALUE_OPTIONAL,'Clear cache.');
@@ -44,12 +37,12 @@ class ContentHubLiftVersion extends Command  implements PlatformBootStrapCommand
    * {@inheritdoc}
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $module_list = \Drupal::getContainer()->get('extension.list.module');
+    $module_list = $this->drupalServiceFactory->getDrupalService('extension.list.module');
     if (!$module_list->exists('acquia_lift')) {
       return 1;
     }
 
-    if (!\Drupal::moduleHandler()->moduleExists('acquia_lift')) {
+    if (!$this->drupalServiceFactory->getDrupalService('module_handler')->moduleExists('acquia_lift')) {
       return 2;
     }
 
@@ -65,14 +58,14 @@ class ContentHubLiftVersion extends Command  implements PlatformBootStrapCommand
     $output->writeln($this->toJsonSuccess([
       'module_version' => $module_list->exists('acquia_lift_publisher') ? 4 : 3,
       'configured' => $this->isAcquiaLiftConfigured(),
-      'base_url' => $input->getOption('uri'),
+      'base_url' => $input->hasOption('uri') ? $input->getOption('uri') : '',
     ]));
 
     return 0;
   }
 
   protected function isAcquiaLiftConfigured(): bool {
-    $config = \Drupal::configFactory()->getEditable('acquia_lift.settings');
+    $config = $this->drupalServiceFactory->getDrupalService('config.factory')->getEditable('acquia_lift.settings');
     return empty($config->get('credential.account_id')) ? FALSE : TRUE;
   }
 
---
+++ tests/Command/ContentHubLiftVersionTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Acquia\Console\ContentHub\Tests\Command;
+
+use Acquia\Console\ContentHub\Command\ContentHubLiftVersion;
+use Acquia\Console\ContentHub\Tests\ContentHubCommandTestBase;
+use Prophecy\Argument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Class ContentHubLiftVersionTest.
+ *
+ * @coversDefaultClass \Acquia\Console\ContentHub\Command\ContentHubLiftVersion
+ *
+ * @group acquia-console-contenthub
+ *
+ * @package Acquia\Console\ContentHub\Tests\Command
+ */
+class ContentHubLiftVersionTest extends ContentHubCommandTestBase {
+
+  /**
+   * Test lift version module.
+   *
+   * @covers ::execute
+   *
+   * @param array $module_exist
+   *   Array containing expected return value of exist method for each module.
+   * @param bool $module_enabled
+   *   Expected return value of moduleExists() method.
+   * @param string $lift_account_id
+   *   Expected return value of lift credential account id.
+   * @param string $needle
+   *   String to look for within display.
+   * @param int $exit_code
+   *   Expected return value of execute() method.
+   *
+   * @dataProvider dataProvider
+   */
+  public function testContentHubLiftVersion(array $module_exist, bool $module_enabled, string $lift_account_id, string $needle, int $exit_code) {
+    $this
+      ->drupalServiceFactory
+      ->getDrupalService(Argument::any())
+      ->shouldBeCalled()
+      ->willReturn($this->getDrupalServiceMocks($module_exist, $module_enabled, $lift_account_id));
+
+    $command = $this->getCommand();
+    $command->setDrupalServiceFactory($this->drupalServiceFactory->reveal());
+    $command->setAchClientService($this->contentHubService->reveal());
+
+    /** @var \Symfony\Component\Console\Tester\CommandTester $command_tester */
+    $command_tester = $this->doRunCommand($command, [], ['alias' => 'test', '--uri' => 'dev.acquiacloud.com']);
+    $this->assertStringContainsString($needle, $command_tester->getDisplay());
+    $this->assertEquals($exit_code, $command_tester->getStatusCode());
+
+  }
+
+  /**
+   * Returns mock instance for getDrupalService().
+   *
+   * @param array $module_exist
+   *   Array containing return value of exists() for each module.
+   * @param bool $module_enabled
+   *   Return value for moduleExists().
+   * @param string $lift_account_id
+   *   Return value for lift credential account id.
+   *
+   * @return object
+   *   Mock of getDrupalService function return.
+   */
+  public function getDrupalServiceMocks(array $module_exist, bool $module_enabled, string $lift_account_id): object {
+    return new class ($module_exist, $module_enabled, $lift_account_id) {
+      public function __construct(array $module_exist, bool $module_enabled, string $lift_account_id) {
+        $this->module_exist = $module_exist;
+        $this->enabled = $module_enabled;
+        $this->lift_account_id = $lift_account_id;
+       }
+
+      public function exists(string $module_name): bool {return $this->module_exist[$module_name];}
+
+      public function moduleExists(): bool {return $this->enabled;}
+
+      public function getEditable(): object {return $this;}
+
+      public function get(): string {return $this->lift_account_id;}
+
+    };
+  }
+
+  /**
+   * A data provider for ::testContentHubLiftVersion()
+   *
+   * @return array[]
+   */
+  public function dataProvider() {
+    return [
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => TRUE],
+        TRUE,
+        'a123er4w3e4',
+        '{"success":true,"data":{"module_version":4,"configured":true,"base_url":"dev.acquiacloud.com"}}',
+        0,
+      ],
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => FALSE],
+        TRUE,
+        'a123er4w3e4',
+        '{"success":true,"data":{"module_version":3,"configured":true,"base_url":"dev.acquiacloud.com"}}',
+        0,
+      ],
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => TRUE],
+        TRUE,
+        '',
+        '',
+        3,
+      ],
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => FALSE],
+        TRUE,
+        '',
+        '',
+        3,
+      ],
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => TRUE],
+        FALSE,
+        '',
+        '',
+        2,
+      ],
+      [
+        ['acquia_lift' => TRUE, 'acquia_lift_publisher' => FALSE],
+        FALSE,
+        '',
+        '',
+        2,
+      ],
+      [
+        ['acquia_lift' => FALSE, 'acquia_lift_publisher' => FALSE],
+        TRUE,
+        '',
+        '',
+        1,
+      ],
+      [
+        ['acquia_lift' => FALSE, 'acquia_lift_publisher' => TRUE],
+        FALSE,
+        '',
+        '',
+        1,
+      ],
+      [
+        ['acquia_lift' => FALSE, 'acquia_lift_publisher' => FALSE],
+        FALSE,
+        '',
+        '',
+        1,
+      ]
+    ];
+  }
+
+  /**
+   * Helper method to get object of ContentHubLiftVersion Command with URI InputOption.
+   *
+   * @return \Acquia\Console\ContentHub\Command\ContentHubLiftVersion
+   */
+  private function getCommand() : ContentHubLiftVersion {
+    return new class extends ContentHubLiftVersion {
+      protected function configure() {
+        parent::configure();
+        $this->addOption('uri', NULL, InputOption::VALUE_OPTIONAL, 'The url from which to mock a request.');
+      }
+    };
+  }
+
+}
