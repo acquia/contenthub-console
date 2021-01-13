f4e4a81fc14387a4924852d0ef8e1537c2457ad8
Alejandro Barrios <alejandro.barrios@acquia.com>
2021-01-13 12:02:58 -0500

    LCH-4692: Fixing use clauses

--- src/Command/Helpers/DrushWrapper.php
+++ src/Command/Helpers/DrushWrapper.php
@@ -2,6 +2,7 @@
 
 namespace Acquia\Console\ContentHub\Command\Helpers;
 
+use Acquia\Console\Helpers\Command\PlatformCmdOutputFormatterTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
