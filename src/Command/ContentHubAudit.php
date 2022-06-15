03d6c98a67739e50c0f1f9c4db1d471531c062a9
Narendra Desai <shenvidesainarendra@gmail.com>
2022-06-15 16:20:27 +0530

    LCH-6076: PQ command code check.

--- src/Command/ContentHubAudit.php
+++ src/Command/ContentHubAudit.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ContentHubAudit extends Command implements PlatformBootStrapCommandInterface {
 
   use CommandExecutionTrait;
+  use ContentHubAuditTrait;
 
   /**
    * {@inheritdoc}
@@ -122,21 +123,10 @@ class ContentHubAudit extends Command implements PlatformBootStrapCommandInterfa
       $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator("$directory/modules"));
       $regex = new \RegexIterator($iterator, '/^.+\.module$/i', \RecursiveRegexIterator::GET_MATCH);
 
-      $hooks = [
-        "acquia_contenthub_drupal_to_cdf_alter",
-        "acquia_contenthub_cdf_from_drupal_alter",
-        "acquia_contenthub_cdf_from_hub_alter",
-        "acquia_contenthub_drupal_from_cdf_alter",
-        "acquia_contenthub_exclude_fields_alter",
-        "acquia_contenthub_field_type_mapping_alter",
-        "acquia_contenthub_cdf_alter",
-        "acquia_contenthub_is_eligible_entity",
-      ];
-
       foreach ($regex as $module_file) {
         $functions = $this->getModuleFunctions($module_file[0]);
         $file_info = pathinfo($module_file[0]);
-        foreach ($hooks as $hook) {
+        foreach ($this->hooks as $hook) {
           if (array_search("{$file_info['filename']}_$hook", $functions) !== FALSE) {
             $ok = FALSE;
             $output->writeln(sprintf("The %s module implements ContentHub 1.x hook %s and must be converted before upgrading to 2.x.", $file_info['filename'], $hook));
@@ -150,70 +140,5 @@ class ContentHubAudit extends Command implements PlatformBootStrapCommandInterfa
     }
   }
 
-  /**
-   * Tokenizes module functions.
-   *
-   * @todo considering genericizing this in another class.
-   *
-   * @param string $file
-   *   The file to tokenize.
-   *
-   * @return array
-   *   Array.
-   */
-  protected function getModuleFunctions(string $file): array {
-    $source = file_get_contents($file);
-    $tokens = token_get_all($source);
-
-    $functions = [];
-    $nextStringIsFunc = FALSE;
-    $inClass = FALSE;
-    $bracesCount = 0;
-
-    foreach ($tokens as $token) {
-      switch ($token[0]) {
-        case T_CLASS:
-          $inClass = TRUE;
-          break;
-
-        case T_FUNCTION:
-          if (!$inClass) {
-            $nextStringIsFunc = TRUE;
-          }
-          break;
-
-        case T_STRING:
-          if ($nextStringIsFunc) {
-            $nextStringIsFunc = FALSE;
-            $functions[] = $token[1];
-          }
-          break;
-
-        // Anonymous functions.
-        case '(':
-        case ';':
-          $nextStringIsFunc = FALSE;
-          break;
-
-        // Exclude Classes.
-        case '{':
-          if ($inClass) {
-            $bracesCount++;
-          }
-          break;
-
-        case '}':
-          if ($inClass) {
-            $bracesCount--;
-            if ($bracesCount === 0) {
-              $inClass = FALSE;
-            }
-          }
-          break;
-      }
-    }
-
-    return $functions;
-  }
 
 }
---
+++ src/Command/ContentHubAuditTrait.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Acquia\Console\ContentHub\Command;
+
+/**
+ * ContentHub audit command related functions.
+ */
+trait ContentHubAuditTrait {
+
+  /**
+   * Hooks provided by CH 1.x module.
+   *
+   * @var array
+   */
+  protected array $hooks = [
+    "acquia_contenthub_drupal_to_cdf_alter",
+    "acquia_contenthub_cdf_from_drupal_alter",
+    "acquia_contenthub_cdf_from_hub_alter",
+    "acquia_contenthub_drupal_from_cdf_alter",
+    "acquia_contenthub_exclude_fields_alter",
+    "acquia_contenthub_field_type_mapping_alter",
+    "acquia_contenthub_cdf_alter",
+    "acquia_contenthub_is_eligible_entity",
+  ];
+
+  /**
+   * Tokenizes module functions.
+   *
+   * @todo considering genericizing this in another class.
+   *
+   * @param string $file
+   *   The file to tokenize.
+   *
+   * @return array
+   *   Array.
+   */
+  protected function getModuleFunctions(string $file): array {
+    $source = file_get_contents($file);
+    $tokens = token_get_all($source);
+
+    $functions = [];
+    $nextStringIsFunc = FALSE;
+    $inClass = FALSE;
+    $bracesCount = 0;
+
+    foreach ($tokens as $token) {
+      switch ($token[0]) {
+        case T_CLASS:
+          $inClass = TRUE;
+          break;
+
+        case T_FUNCTION:
+          if (!$inClass) {
+            $nextStringIsFunc = TRUE;
+          }
+          break;
+
+        case T_STRING:
+          if ($nextStringIsFunc) {
+            $nextStringIsFunc = FALSE;
+            $functions[] = $token[1];
+          }
+          break;
+
+        // Anonymous functions.
+        case '(':
+        case ';':
+          $nextStringIsFunc = FALSE;
+          break;
+
+        // Exclude Classes.
+        case '{':
+          if ($inClass) {
+            $bracesCount++;
+          }
+          break;
+
+        case '}':
+          if ($inClass) {
+            $bracesCount--;
+            if ($bracesCount === 0) {
+              $inClass = FALSE;
+            }
+          }
+          break;
+      }
+    }
+
+    return $functions;
+  }
+
+}
